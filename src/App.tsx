91d7020ea5056788b41470bbcae5e63c80279b7c
USERNAME <shinozakiyume88@gmail.com>
2023-03-27 12:45:37 +0900

    返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Practice1 />
+      <Practice2 />
     </div>
   );
 }
